653aa90e52128b827f73e1c15b8d09d98e538f2d
Jed Walton <jedwaltondev@gmail.com>
2023-08-16 16:37:23 +0100

    init

---
+++ pages/_app.tsx
@@ -0,0 +1,17 @@
+// _app.tsx
+import '../css/globals.css'
+import { AppProps } from 'next/app'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <div className={inter.className}>
+      <Component {...pageProps} />
+    </div>
+  )
+}
+
+export default MyApp
+
