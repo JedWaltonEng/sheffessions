f585a33c14b98095b0c8b980f24133f0aaf6d811
Jed Walton <jedwaltondev@gmail.com>
2023-08-16 17:44:48 +0100

    test

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,16 +5,17 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>
-          <link rel="icon" type="image/png" href="/logo.png" />
-          {/* Optionally, add other favicon formats */}
-          {/* <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" /> */}
-          {/* <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" /> */}
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
+          <Head>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+            <link rel="icon" type="image/png" href="/logo.png" />
+            {/* Optionally, add other favicon formats */}
+            {/* <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" /> */}
+            {/* <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" /> */}
+          </Head>
+          <body>
+            <Main />
+            <NextScript />
+          </body>
       </Html>
     );
   }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,19 @@
 export default function Home() {
   return (
     <>
-      <main className="main-content">
+      <main className="flex min-h-screen flex-col items-center justify-center p-24">
         <div className="z-10 max-w-5xl w-full flex flex-col items-center justify-center font-mono text-sm lg:flex">
-          <img 
-              src="/logo.png" 
-              alt="Company Logo" 
-              className="logo" 
+          <img
+            src="/logo.png"
+            alt="Company Logo"
+            className="logo"
           />
           <p className="mb-4 text-xl font-bold">Sheffessions_</p>
           <textarea
             rows={5}
             placeholder="Type your anonymous confession here..."
-            className="w-full h-64 p-6 border-2 rounded-md text-xl mb-4 md:w-3/4 lg:w-2/3"
+            className="w-11/12 h-48 p-6 border-2 rounded-md text-xl mb-4 md:w-3/4 lg:w-2/3"
           ></textarea>
-
           <button className="p-2 bg-blue-500 text-white rounded-md">
             Submit
           </button>
