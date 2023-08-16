8cd4467977df4f1392fb0df9e3127da8948b93ed
Jed Walton <jedwaltondev@gmail.com>
2023-08-16 17:27:24 +0100

    basics

---
+++ pages/_document.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="icon" type="image/png" href="/logo.png" />
+          {/* Optionally, add other favicon formats */}
+          {/* <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" /> */}
+          {/* <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" /> */}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
+
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,18 @@
-
 export default function Home() {
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <main className="main-content">
         <div className="z-10 max-w-5xl w-full flex flex-col items-center justify-center font-mono text-sm lg:flex">
+          <img 
+              src="/logo.png" 
+              alt="Company Logo" 
+              className="logo" 
+          />
           <p className="mb-4 text-xl font-bold">Sheffessions_</p>
           <textarea
             rows={5}
             placeholder="Type your anonymous confession here..."
-            className="w-full md:w-1/2 lg:w-1/1.5 h-24 p-4 border-2 rounded-md text-lg mb-4"
+            className="w-full h-64 p-6 border-2 rounded-md text-xl mb-4 md:w-3/4 lg:w-2/3"
           ></textarea>
 
           <button className="p-2 bg-blue-500 text-white rounded-md">
