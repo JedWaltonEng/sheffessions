01fb0645acc6a2c274ac9071a33e7de7e1128239
Jed Walton <jedwaltondev@gmail.com>
2023-08-22 17:00:45 +0100

    +

--- frontend/sheffessions/next.config.js
+++ frontend/sheffessions/next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   output: 'standalone',
   env: {
     ENV: process.env.ENV,
-    NEXT_PUBLIC_ENV: process.env.NEXT_PUBLIC_ENV
+    NEXT_PUBLIC_ENV: process.env.NEXT_PUBLIC_ENV,
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL
   }
 }
