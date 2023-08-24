9fedce37c7014dd91d8d4144bed4dd1ebc9c8821
Jed Walton <jedwaltondev@gmail.com>
2023-08-25 00:49:28 +0100

    this should work now

--- backend/sheffessions_api/db/migrations/000001_create_confessions_table_dupe.down.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE confessions (
-    id SERIAL PRIMARY KEY,
-    confession_text TEXT NOT NULL,
-    date_of_confession DATE NOT NULL DEFAULT CURRENT_DATE,
-    source_of_confession VARCHAR(255) NOT NULL
-);
