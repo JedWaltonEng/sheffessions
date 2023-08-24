d6e528664b23cd4ab3cb4a548c91842163afdace
Jed Walton <jedwaltondev@gmail.com>
2023-08-24 20:56:56 +0100

    +

--- backend/sheffessions_api/db/migrations/000001_problematic.down.sql
+++
@@ -1,2 +0,0 @@
-DROP TABLE confessions_problematic;
-
--- backend/sheffessions_api/db/migrations/000001_problematic.up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE confessions_problematic (
-    id SERIAL PRIMARY KEY,
-    confession_text TEXT NOT NULL,
-    date_of_confession DATE NOT NULL DEFAULT CURRENT_DATE,
-    source_of_confession VARCHAR(255) NOT NULL
-);
-
