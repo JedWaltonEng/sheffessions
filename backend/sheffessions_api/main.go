95f37cb22c633a3ddc0c42b173a0b0addd48706f
Jed Walton <jedwaltondev@gmail.com>
2023-08-28 00:21:05 +0100

    handlers/confessions added

--- backend/sheffessions_api/main.go
+++ backend/sheffessions_api/main.go
@@ -1,66 +1,15 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
 	"os"
+	"sheffessions/api/handlers"
 	"sheffessions/api/middleware"
 	"sheffessions/api/store"
 
 	_ "github.com/lib/pq"
 )
-
-// func loggingMiddleware(next http.HandlerFunc) http.HandlerFunc {
-// 	return func(w http.ResponseWriter, r *http.Request) {
-// 		log.Printf("Received %s request for %s from %s\n", r.Method, r.URL.Path, r.RemoteAddr)
-// 		next(w, r)
-// 		log.Println("Handled the request.")
-// 	}
-// }
-
-type Confession struct {
-	Content            string `json:"content"`
-	SourceOfConfession string `json:"source_of_confession"` // added this line
-}
-
-var confessions []Confession
-
-func handleConfessions(w http.ResponseWriter, r *http.Request) {
-	enableCors(&w)
-	// Handle preflight request (for CORS)
-	if r.Method == http.MethodOptions {
-		w.WriteHeader(http.StatusOK)
-		return
-	}
-
-	if r.Method == http.MethodPost {
-		var confession Confession
-		if err := json.NewDecoder(r.Body).Decode(&confession); err != nil {
-			http.Error(w, "Failed to decode confession", http.StatusBadRequest)
-			return
-		}
-		confessions = append(confessions, confession)
-		// Save to PostgreSQL
-		result, err := store.DB.Exec("INSERT INTO confessions (confession_text, source_of_confession) VALUES ($1, $2)", confession.Content, confession.SourceOfConfession)
-		if err != nil {
-			log.Println("Failed to insert confession to database:", err)
-			http.Error(w, "Failed to save confession", http.StatusInternalServerError)
-			return
-		}
-		affected, _ := result.RowsAffected()
-		log.Printf("Inserted confession into database. Rows affected: %d", affected)
-
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("Confession received"))
-		log.Println("Confession received: ", confession)
-
-	} else if r.Method == http.MethodGet {
-		json.NewEncoder(w).Encode(confessions)
-	} else {
-		http.Error(w, "Method not supported", http.StatusMethodNotAllowed)
-	}
-}
 
 func enableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
@@ -72,7 +21,7 @@ func main() {
 	store.InitDB()
 	defer store.DB.Close()
 
-	http.HandleFunc("/confessions", middleware.Logging(handleConfessions))
+	http.HandleFunc("/confessions", middleware.Logging(handlers.HandleConfessions))
 
 	port := os.Getenv("PORT")
 	if port == "" {
